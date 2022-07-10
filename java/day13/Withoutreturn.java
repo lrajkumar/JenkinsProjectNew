b951473c023fd319b0878166bce42c3f034c0be5
janasanthosh016 <janasanthosh016@gmail.com>
2022-07-10 14:29:09 +0530

    first commit

---
+++ java/day13/Withoutreturn.java
@@ -0,0 +1,21 @@
+package day13;
+
+public class Withoutreturn {
+	public String print() {
+		return "WELCOME";
+	}
+	public String printnum() {
+		return "The num is"+10;
+	}
+	public int sum() {
+		int a=10;
+		int b=20;
+		return a+b;
+	}
+	public String add() {
+		int a=10;
+		int b=30;
+		return "THE SUM IS"+(a+b);
+	}
+
+}
